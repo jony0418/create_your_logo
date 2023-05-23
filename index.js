3adb7bef58ed3aad8c68914298f9ae404d1c889c
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-05-22 22:56:16 -0600

    added comments to the index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+//This is the main file for the logo generator. It will prompt the user for input, then generate a logo based on the input. The logo will be saved in the current directory as logo.svg.
 const inquirer = require('inquirer');
 const fs = require('fs');
 const { Circle, Triangle, Square } = require('./lib/shapes.js');
 
+//Here I use the inquirer package to prompt the user for input. The answers are then used to create a new shape object and a new SVG file. 
+//The SVG file is saved in the current directory as logo.svg.
 inquirer
   .prompt([
     {
@@ -27,6 +30,7 @@ inquirer
       message: 'Enter a color keyword or a hexadecimal number for the shape color:'
     }
   ])
+//Here I use a switch statement to create a new shape object based on the user's input.  
   .then((answers) => {
     let shape;
     switch (answers.shape) {
@@ -40,14 +44,14 @@ inquirer
         shape = new Square(answers.shapeColor);
         break;
     }
-    
+    //Here I use a template literal to create an SVG file based on the user's input. The SVG file is saved in the current directory as logo.svg.
     const svg = `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
       ${shape.render()}
       <text x="50%" y="50%" font-size="40" text-anchor="middle" dominant-baseline="middle" fill="${answers.textColor}">${answers.text}</text>
     </svg>
   `;
-
+//Here I use the fs package to write the SVG file to the current directory as logo.svg.
     fs.writeFile('logo.svg', svg, (err) => {
       if (err) throw err;
       console.log('Generated logo.svg');
